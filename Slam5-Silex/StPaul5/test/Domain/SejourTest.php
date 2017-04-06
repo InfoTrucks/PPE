5f584cd41f1c1b8e5340e95871f9e6ee8bb97ac4
15hellioj <j-hellio@outlook.fr>
2017-04-06 11:10:38 +0200

    ()

--- Slam5-Silex/StPaul5/test/Domain/SejourTest.php
+++ Slam5-Silex/StPaul5/test/Domain/SejourTest.php
@@ -41,7 +41,7 @@ class SejourTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($resultatAttendu, $resultatObserve);
 
     }
-    public function testSimul{
+    public function testSimul(){
 
     }
 }
