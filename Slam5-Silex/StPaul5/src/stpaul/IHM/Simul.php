5936eebaa5bad26bafbe75adf0ee9c5731ab0bc4
15milletl <lulumlt29@gmail.com>
2017-04-06 11:14:00 +0200

    Ajout class simul

---
+++ Slam5-Silex/StPaul5/src/stpaul/IHM/Simul.php
@@ -0,0 +1,19 @@
+class Simul {
+
+private $famNom;
+private $famnbEnfant;
+private $famQF;
+
+private $sejNo;
+private $sejMBI;
+
+private $simulNbEnfPartant;
+private $simulReducQF;
+private $simulReducFamilleNombreuse;
+private $simulReducDepartMultiple;
+private $simulSousTotal;
+private $simulTotalApresReduc;
+private $simulTotalApresPlafond;
+private $simulTotalDepartMultiple;
+
+}
